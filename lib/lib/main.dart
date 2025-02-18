0cc85dae5b0e7cef4c3512af39b87167d26d695b
Weeraphat-T <s6503051624068@email.kmutnb.ac.th>
2025-02-18 16:12:18 +0700

    18-2-2025-2
    lololol

---
+++ lib/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mytravel/screens/signin_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:mytravel/screens/onboarding_screen.dart';
+
+bool show = true;
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final prefs = await SharedPreferences.getInstance();
+  show = prefs.getBool('ON BOARDING') ?? true;
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(textTheme: GoogleFonts.lexendTextTheme()),
+      title: 'My Travel 1.0',
+      home: show ? const OnboardingScreen() : SigninPage(),
+    );
+  }
+}
---
+++ lib/lib/widgets/icon_widget.dart
@@ -0,0 +1,43 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:mytravel/constants/colors.dart';
+
+class IconWidget extends StatelessWidget {
+  const IconWidget({
+    super.key,
+    required this.title,
+    required this.color,
+    required this.icon,
+  });
+
+  final String title;
+  final Color color;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 50,
+          width: 50,
+          decoration: BoxDecoration(
+            color: color,
+            shape: BoxShape.circle,
+          ),
+          padding: const EdgeInsets.all(8.0),
+          child: Icon(icon,
+              size: 30, color: primaryColors),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Text(
+          title,
+          style: TextStyle(
+              fontSize: 16, fontWeight: FontWeight.w500),
+        ),
+      ],
+    );
+  }
+}
